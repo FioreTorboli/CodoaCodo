1b79564ecaa612c83bf4107eaa42c79e71e4e405
FioreTorboli <fiorella-torboli@hotmail.com>
2024-05-24 12:56:58 -0300

    ult

--- codigo-landing/headerFooter.js
+++ codigo-landing/headerFooter.js
@@ -3,42 +3,44 @@ const footer = document.querySelector("footer");
 
 header.innerHTML = `
 <div class="header">
-            <a href="./index.html"><img class="logo-color" src="./img/1LOGO.png" alt="logo"></a>
-            <nav>
-                <ul>
-                    <li class="link-nav">
-                        <a href="./sobre_nosotros.html">Sobre nosotros</a>
-                    </li>
-                    <li class="link-nav">
-                        <a href="#">Cursos</a>
-                    </li>
-                    <li class="link-carrito">
-                        <a href="#"><img src="./icons/carrito V.png" alt="carrito"></a>
-                    </li>
-                    <li class="inicio-sesion">
-                        <a href="./login.html">Iniciar sesión</a>
-                    </li>
-                    <li>
-                        <a href="./register.html"><button class="btm">REGISTRARSE</button></a>
-                    </li>
-                </ul>
-            </nav>
+    <nav class="nav">
+        <div class="logo">
+            <a href="landing.html"><img src="./img/1LOGO.png" alt="logo"></a>
         </div>
+
+        <input type="checkbox" id="check">
+
+        <label for="check" class="icons">
+            <i class='bx bx-menu' id="menu-icon"></i>
+            <i class='bx bx-x' id="close-icon"></i>
+        </label>
+
+        <div class="nav-bar">
+            
+                <a href="./sobre_nosotros.html">Sobre nosotros</a>
+                <a href="#">Cursos</a>
+                <a href="#"><img src="./img/carrito V.png" alt=""></a>
+                <a href="./login.html">Iniciar sesion</a>
+                <a href="./register.html"><button class="btm">REGISTRARSE</button></a>
+            
+        </div>
+    </nav>
+</div>
 `;
 
 footer.innerHTML = `
 <div class="footer">
-<div class="left">
-    <img src="./img/1logo-blanco.png" alt="">
-    <p>Políticas de Privacidad | Términos y Condiciones</p>
-</div>
-<div class="right">
-    <p>Buscanos en redes</p>
-    <div class="icon-redes">
-        <a href="#"><img class="ig" src="./icons/instagram-logo-240.png" alt="instagram"></a>
-        <a href="#"><img class="in" src="./icons/linkedin-logo-240.png" alt="linkedin"></a>
-    </div>
-</div>
+        <div class="left">
+            <img src="./img/1logo-blanco.png" alt="">
+            <p>Políticas de Privacidad | Términos y Condiciones</p>
+        </div>
+        <div class="right">
+            <p>Buscanos en redes</p>
+            <div class="icon-redes">
+                <a href="#"><img class="ig" src="./img/instagram-logo-240.png" alt="instagram"></a>
+                <a href="#"><img class="in" src="./img/linkedin-logo-240.png" alt="linkedin"></a>
+            </div>
+        </div>
 
-</div>
+    </div>
 `;
